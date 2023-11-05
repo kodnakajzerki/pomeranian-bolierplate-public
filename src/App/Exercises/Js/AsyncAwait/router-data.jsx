d358cb6359fddd67b03b7ae4bcaa16e0efa29a52
Patrycja <patrycja.ojewska@gmail.com>
2023-11-05 12:42:35 +0100

    feat: added fetch logic using promises

---
+++ src/App/Exercises/Js/AsyncAwait/router-data.jsx
@@ -0,0 +1,18 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'async-await',
+  date: null,
+  linkLabel: 'JS - Async/Await',
+  blockNo: 34,
+  element: <Exercise />,
+  tags: [
+    'async',
+    'await',
+    'try',
+    'catch',
+    'useeffect',
+    'asynchronous',
+    'javascript',
+  ],
+};
---
+++ src/App/Exercises/Js/TryCatchFinally/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'try-catch-finally',
+  date: null,
+  linkLabel: 'JS - Try/Catch/Finally',
+  blockNo: 34,
+  element: <Exercise />,
+  tags: ['try', 'catch', 'finally', 'javascript'],
+};
