cecd45f1275fd06a6bb3c46cdb610c4514aaf4a5
Patrycja <patrycja.ojewska@gmail.com>
2023-11-12 11:27:51 +0100

    ToDoWithServer

--- src/App/Exercises/ReactExercises/ToDoWithServer2/ToDoWithServer.jsx
+++ src/App/Exercises/ReactExercises/ToDoWithServer2/ToDoWithServer.jsx
@@ -1,224 +1,30 @@
 import { useState, useEffect } from 'react';
-const API_URL = 'http://localhost:3333/api/todo';
-const TEXTS = {
-  LOADER: 'Ładowanie danych...',
-  ERROR: 'Przepraszamy. Nie udało się pobrać listy zadań.',
-  EMPTY: 'Brawo! Nie masz aktualnie żadnych zadań do zrealizowania.',
-  BUTTON_REFRESH: 'ODSWIEŻ WIDOK',
-};
 
-const apiRequest = async (
-  url,
-  {
-    id = null,
-    method = 'GET',
-    body = null,
-    loadingHandler,
-    errorHandler,
-    errorMessage = '',
-    updateHandler,
-  }
-) => {
-  loadingHandler(true);
-  errorHandler(false);
+import { Todo } from './components/Todo';
+import { FormTodo } from './components/FormTodo';
 
-  try {
-    let fetchOptions = null;
+import { apiRequest } from './helpers';
 
-    if (method !== 'GET') {
-      fetchOptions = {
-        method, // method: method
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-    }
-
-    if (body) {
-      fetchOptions.body = JSON.stringify(body);
-    }
-
-    const response = await fetch(url, fetchOptions);
-
-    if (!response.ok) {
-      throw new Error(errorMessage);
-    }
-
-    const data = await response.json();
-
-    updateHandler(data);
-  } catch (error) {
-    if (id) {
-      errorHandler(id);
-    } else {
-      errorHandler(errorMessage);
-    }
-  } finally {
-    loadingHandler(false);
-  }
-};
+import { API_URL, TEXTS } from './constants';
 
 export function Exercise() {
   const [todos, setTodos] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-
-  // TODO:
-  // GENERIC ERROR
-  // const [genericError, setGenericError] = useState(false);
-  // [
-  //   {
-  //     id: 1,
-  //     errors: [
-  //       {
-  //         type: 'DELETE',
-  //         message: 'message',
-  //       },
-  //       {
-  //         type: 'MARK_AS_DONE',
-  //         message: 'message',
-  //       },
-  //     ],
-  //   },
-  // ];
-
-  // DELETE
-  const [deleteLoading, setDeleteLoading] = useState(false);
-  const [deleteErrorId, setDeleteErrorId] = useState(null);
-
-  // MARK AS DONE
-  const [markLoading, setMarkLoading] = useState(false);
-  const [markErrorId, setMarkErrorId] = useState(null);
-
-  // FORM
   const [isForm, setIsForm] = useState(false);
-
-  // HANDLING FORM
-  const [formLoading, setFormLoading] = useState(false);
-  const [formErrorId, setFormErrorId] = useState(null);
-  const [editedId, setEditedId] = useState(null);
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [note, setNote] = useState('');
-
-  const sendDisabled = !title || !author || !note;
-
-  const [isEditForm, setEditForm] = useState(false);
+  const [editedItem, setEditedItem] = useState(null);
 
   const fetchTodos = () => {
     apiRequest(API_URL, {
       loadingHandler: setLoading,
       errorHandler: setError,
       updateHandler: setTodos,
-      errorMessage: 'Failed to load data!',
+      errorMessage: TEXTS.API.ERRORS.GET_DATA,
     });
-  };
-
-  const deleteTodo = (id) => {
-    apiRequest(`${API_URL}/${id}`, {
-      method: 'DELETE',
-      id,
-      loadingHandler: setDeleteLoading,
-      errorHandler: setDeleteErrorId,
-      updateHandler: (data) => {
-        setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
-      },
-      errorMessage: 'Failed to delete data!',
-    });
-  };
-
-  const markAsDone = (id) => {
-    apiRequest(`${API_URL}/${id}/markAsDone`, {
-      method: 'PUT',
-      id,
-      loadingHandler: setMarkLoading,
-      errorHandler: setMarkErrorId,
-      updateHandler: (data) => {
-        setTodos((prevState) =>
-          prevState.map((todo) => {
-            if (todo.id === id) return data;
-
-            return todo;
-          })
-        );
-      },
-      errorMessage: 'Failed to mark as done!',
-    });
-  };
-
-  const addTodo = () => {
-    apiRequest(API_URL, {
-      method: 'POST',
-      loadingHandler: setFormLoading,
-      errorHandler: setFormErrorId,
-      body: { title, author, note },
-      updateHandler: (data) => {
-        setTodos((prevState) => [...prevState, data]);
-        handleFormVisibility();
-        setTitle('');
-        setAuthor('');
-        setNote('');
-      },
-      errorMessage: 'Failed add!',
-    });
-  };
-
-  const editTodo = () => {
-    apiRequest(`${API_URL}/${editedId}`, {
-      method: 'PUT',
-      id: editedId,
-      loadingHandler: setFormLoading,
-      errorHandler: setFormErrorId,
-      body: { title, author, note },
-      updateHandler: (data) => {
-        setTodos((prevState) =>
-          prevState.map((todo) => {
-            if (todo.id === editedId) return data;
-
-            return todo;
-          })
-        );
-        setEditedId(null);
-        setEditForm(false);
-        setTitle('');
-        setAuthor('');
-        setNote('');
-        handleFormVisibility();
-      },
-      errorMessage: 'Failed edit!',
-    });
-  };
-
-  const handleAddToDo = (e) => {
-    e.preventDefault();
-    if (isEditForm) {
-      editTodo();
-    } else {
-      addTodo();
-    }
   };
 
   const handleFormVisibility = () => {
     setIsForm(!isForm);
-  };
-
-  const handleEdit = (todo) => {
-    setEditForm(true);
-    setEditedId(todo.id);
-    setTitle(todo.title);
-    setAuthor(todo.author);
-    setNote(todo.note);
-    handleFormVisibility();
-  };
-
-  const handleBack = () => {
-    handleFormVisibility();
-    setTitle('');
-    setAuthor('');
-    setNote('');
-    setEditForm(false);
-    setEditedId(null);
-    setFormErrorId(null);
   };
 
   useEffect(() => {
@@ -242,7 +48,7 @@ export function Exercise() {
     return (
       <div>
         <div>{TEXTS.EMPTY}</div>
-        <button onClick={handleFormVisibility}>Dodaj zadanie</button>
+        <button onClick={handleFormVisibility}>{TEXTS.BUTTON_ADD}</button>
         <button onClick={fetchTodos}>{TEXTS.BUTTON_REFRESH}</button>
       </div>
     );
@@ -250,88 +56,29 @@ export function Exercise() {
 
   return (
     <>
-      <h2>Blok 38 - ToDo</h2>
+      <h2>{TEXTS.TITLE}</h2>
       {isForm && (
-        <form onSubmit={handleAddToDo}>
-          <div>
-            <label htmlFor="title">Tytuł</label>
-            <input
-              id="title"
-              name="title"
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="author">Autor</label>
-            <input
-              id="author"
-              name="author"
-              type="text"
-              value={author}
-              onChange={(e) => setAuthor(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="note">Treść</label>
-            <textarea
-              id="note"
-              name="note"
-              type="text"
-              value={note}
-              onChange={(e) => setNote(e.target.value)}
-            />
-          </div>
-          <div>
-            <button onClick={handleBack}>COFNIJ</button>
-            <button disabled={formLoading || sendDisabled} type="submit">
-              {formLoading ? 'WYSYŁANIE...' : isEditForm ? 'EDYTUJ' : 'DODAJ'}
-            </button>
-          </div>
-          {formErrorId && <div>Nie udało się dodać zadania</div>}
-        </form>
+        <FormTodo
+          editedItem={editedItem}
+          setEditedItem={setEditedItem}
+          handleFormVisibility={handleFormVisibility}
+          // handleBack={handleBack}
+          setTodos={setTodos}
+        />
       )}
       {!isForm && (
         <>
-          <button onClick={handleFormVisibility}>Dodaj zadanie</button>
-          {todos.map((todo) => {
-            return (
-              <div key={todo.id}>
-                <h3>{todo.title}</h3>
-                <p>{todo.note}</p>
-                {!todo.isDone && (
-                  <button
-                    disabled={markLoading}
-                    onClick={() => markAsDone(todo.id)}
-                  >
-                    {markLoading ? 'KOŃCZENIE ZADANIA...' : 'ZAKOŃCZ'}
-                  </button>
-                )}
-
-                <button onClick={() => handleEdit(todo)}>EDYTUJ</button>
-
-                {!deleteLoading ? (
-                  <button onClick={() => deleteTodo(todo.id)}>DELETE</button>
-                ) : (
-                  <div>USUWANIE</div>
-                )}
-
-                {markErrorId === todo.id ? (
-                  <div>nie udało się zakończyć</div>
-                ) : (
-                  ''
-                )}
-
-                {deleteErrorId === todo.id ? (
-                  <div>nie udało się usunąć</div>
-                ) : (
-                  ''
-                )}
-              </div>
-            );
-          })}
-          <button onClick={handleFormVisibility}>Dodaj zadanie</button>
+          <button onClick={handleFormVisibility}>{TEXTS.BUTTON_ADD}</button>
+          {todos.map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              setEditedItem={setEditedItem}
+              handleFormVisibility={handleFormVisibility}
+              setTodos={setTodos}
+            />
+          ))}
+          <button onClick={handleFormVisibility}>{TEXTS.BUTTON_ADD}</button>
         </>
       )}
     </>
