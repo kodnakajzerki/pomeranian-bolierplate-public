96762dd1c8d651c2974e0fbb0bf57d8fc26a98f1
Patrycja <patrycja.ojewska@gmail.com>
2023-11-23 17:59:06 +0100

    firebase

---
+++ src/App/Exercises/ReactExercises/Firebase/router-data.jsx
@@ -0,0 +1,9 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'basic-forms',
+  data: null,
+  linkLabel: 'Elementy podstawowe formularzy',
+  element: <Exercise />,
+  tags: ['form', 'input', 'textarea', 'select', 'radio', 'checkbox'],
+};
